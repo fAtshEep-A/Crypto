5135d1ba2209cd319ba11ec9444781d6b97d6cc9
shEep <80027135+fAtshEep-A@users.noreply.github.com>
2023-05-29 14:10:33 +0800

    Create xor.c

---
+++ xor.c
@@ -0,0 +1,13 @@
+char my_secret_key[] = "yourkey";
+
+
+void XOR(char * data, size_t data_len, char * key, size_t key_len) {
+    int j;
+    j = 0;
+    for (int i = 0; i < data_len; i++) {
+            if (j == key_len - 1) j = 0;
+
+            data[i] = data[i] ^ key[j];
+            j++;
+    }
+}
